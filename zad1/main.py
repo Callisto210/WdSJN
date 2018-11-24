0c775e22536c39d74d0ead78efbaad1891565cd4
Callisto210 <mikro16@o2.pl>
2018-11-24 23:15:43 +0100

    Added file with examples

--- zad1/main.py
+++ zad1/main.py
@@ -3,15 +3,13 @@ from evaluator import NumeralsNormalizer, NumeralsTransformer
 from math import *
 
 if __name__ == "__main__":
-    data = ('''minus otwórz nawias osiemset trzy miliony dwieście osiemdziesiąt sześć tysięcy dziewięćdziesiąt jeden '''
-            '''dodać siedem zamknij nawias podzielić przez otwórz nawias minus cztery koma trzynaście razy minus '''
-            '''e zamknij nawias pomnóż przez dziewięć minus sinus cosinus dziewięćdziesięciu ośmiu minus sinus '''
-            '''jeden razy cosinus pi plus minus otwórz nawias minus czterdzieści pięć zamknij nawias do potęgi '''
-            '''dwa do potęgi minus dwa kropka zero''')
-
     normalizer = NumeralsNormalizer("numerals.txt")
     transformer = NumeralsTransformer(normalizer)
 
-    result = parse(transformer.replace_with_numbers(data))
+    with open("examples.txt") as f:
+        examples = f.readlines()
 
-    print(eval(result))
+    for example in examples:
+        print(example)
+        result = parse(transformer.replace_with_numbers(example))
+        print(str(eval(result)) + "\n")
